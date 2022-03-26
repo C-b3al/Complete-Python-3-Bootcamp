5b275b5623d98b30aca2b44e9c260c379ddf1858
C-b3al <cjb4017@ic.ac.uk>
2022-03-26 12:27:31 +0000

    Comparisons and logical operators

---
+++ 01-Python Comparison Operators/Comparisons and logical operators.py
@@ -0,0 +1,10 @@
+#### comparisons and logical operators
+# comparisons between variables depend on:
+# 1. similarity
+# 2. type 
+
+# possible to chain operations together
+print(1<2<3)
+# can use inbuilt functionality keywords to return booleans
+print(1<2 and 2<3) # both need to be true to return true
+print(1==2 or 2<3) # only one needs to be true to return true
